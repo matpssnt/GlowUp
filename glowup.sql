92d5caae49aa7888430ed34e1be218e1a219d17a
Mateus Possonato <mateuspossonato@gmail.com>
2025-11-07 15:10:53 -0300

    Fix: Alteração do tamanho de senha de cadastro, arquivo sql

--- glowup.sql
+++ glowup.sql
@@ -57,7 +57,7 @@ CREATE TABLE `cadastros` (
   `id` int(11) NOT NULL,
   `nome` varchar(255) NOT NULL,
   `email` varchar(100) NOT NULL,
-  `senha` varchar(50) NOT NULL,
+  `senha` varchar(255) NOT NULL,
   `isProfissional` tinyint(1) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
