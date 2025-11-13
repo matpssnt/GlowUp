2f5399ef7a72fa5b07cbae5480ec8fb0486649ba
Matheus-Ribeiro-Jesus <pessoalsenacsp@gmail.com>
2025-11-13 11:33:23 -0300

    Relacionamento
    Criando um relacionamento entre as tabelas.

--- controllers/CadastroController.php
+++ controllers/CadastroController.php
@@ -1,72 +1,89 @@
 <?php
 
 require_once __DIR__ . '/../models/CadastroModel.php';
+require_once __DIR__ . '/../models/ClientModel.php';
+require_once __DIR__ . '/../models/ProfissionalModel.php';
 require_once __DIR__ . '/../controllers/PasswordController.php';
 require_once __DIR__ . '/../controllers/ValidadorController.php';
+require_once __DIR__ . '/../helpers/response.php';
 
-
-class CadastroController{
-    public static function create($data){
-
+class CadastroController
+{
+    public static function create($data)
+    {
         ValidadorController::validate_data($data, ["nome", "email", "senha", "isProfissional"]);
+
         $data['senha'] = PasswordController::generateHash($data['senha']);
 
-        $resultado = CadastroModel::create($data);
-        if($resultado){
-            return jsonResponse(['message'=> 'Cadastro criado com sucesso'], 200);
-        }else{
-            return jsonResponse(['message'=> 'Erro ao criar um cadastro'], 400);
+        // Criando o cadastro principal..
+        $idCadastro = CadastroModel::create($data);
+
+        if ($idCadastro) {
+            // Criando o relacionamento com a tabela profissionais..
+            if ($data['isProfissional'] == 1) {
+                $profData = [
+                    'nome' =>   $data['nome'],
+                    'email' => $data['email'],
+                    'descricao' => '',
+                    'acessibilidade' => 0,
+                    'isJuridica' => 0,
+                    'id_cadastro_fk' => $idCadastro 
+                ];
+                ProfissionalModel::create($profData);
+            } else {
+                // Criando o relacionamento com a tabela clientes..
+                $clienteData = [
+                    'nome' => $data['nome'],
+                    'id_cadastro_fk' => $idCadastro
+                ];
+                ClientModel::create($clienteData);
+            }
+
+            return jsonResponse(['message' => 'Cadastro criado com sucesso!'], 201);
+        } else {
+            return jsonResponse(['message' => 'Erro ao criar cadastro'], 400);
         }
     }
 
-
-    public static function update($data, $id){
+    public static function update($data, $id)
+    {
         $resultado = CadastroModel::update($data, $id);
-        if($resultado ) {
+        if ($resultado) {
             return jsonResponse(['message' => 'Cadastro atualizado com sucesso'], 200);
-        }else{
-            return jsonResponse(['message'=> 'Falha na atualização do cadastro'], 400);
+        } else {
+            return jsonResponse(['message' => 'Falha na atualização do cadastro'], 400);
         }
     }
 
-    public static function delete($id){
+    public static function delete($id)
+    {
         $result = CadastroModel::delete($id);
         if ($result) {
-            return jsonResponse(['message' => 'Cadastro deletada com sucesso'], 200);
+            return jsonResponse(['message' => 'Cadastro deletado com sucesso'], 200);
         } else {
             return jsonResponse(['message' => 'Erro ao deletar cadastro'], 400);
         }
     }
 
-    public static function getById($id){
+    public static function getById($id)
+    {
         $resultado = CadastroModel::getById($id);
         if ($resultado) {
             return jsonResponse($resultado);
         } else {
-            return jsonResponse(['message' => 'Cadastro não encontrada'], 400);
+            return jsonResponse(['message' => 'Cadastro não encontrado'], 400);
         }
     }
 
-    public static function getAll(){
+    public static function getAll()
+    {
         $resultado = CadastroModel::getAll();
         if (!empty($resultado)) {
             return jsonResponse($resultado);
         } else {
-            return jsonResponse(['message' => 'Nenhuma cadastro encontrada'], 400);
+            return jsonResponse(['message' => 'Nenhum cadastro encontrado'], 400);
         }
     }
-
-    
-
-
-
-
 }
-
-
-
-
-
-
 
 ?>
